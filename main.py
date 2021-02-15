fb59f74f41b7c1243353c9c7825d922cc9d7e360
Tom Victor <tom.victor@admaren.com>
2021-02-15 19:27:30 +0530

    added asyncio gather

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
-
+import asyncio
 app = FastAPI()
 
 
+
+async def func1():
+    print("func1 start")
+    await asyncio.sleep(7)
+    print("func1 end")
+
+async def func2():
+    print("func2 start")
+    await asyncio.sleep(7)
+    print("func2 end")
+
+
+
+
 @app.get("/")
-def root():
+async def root():
     """Home API
 
     Returns:
 
     """
+    await asyncio.gather(func1(), func2())
     resp = {"id": 1, "name": "test"}
     return resp
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,8 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
-print("tt")
 
 def test_read_main():
-    print("test")
     response = client.get("/")
     assert response.status_code == 200
     print(response.json())
