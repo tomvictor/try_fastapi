2c39f3290c61a47bea84a23d5c0c548259fdb422
Tom Victor <vjtomvictor@icloud.com>
2022-04-02 18:45:45 +0530

    add delete logic

--- app/api/v1/endpoints/heroes.py
+++ app/api/v1/endpoints/heroes.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 
 from app.api.deps import get_db
-from app.models.hero import HeroRead, HeroCreate
+from app.models.hero import HeroRead, HeroCreate, Hero, HeroUpdate
 from app import crud
 
 router = APIRouter()
@@ -14,6 +14,32 @@ def read_root():
 
 
 @router.post("/heroes/", response_model=HeroRead)
-def create_hero(*, session: Session = Depends(get_db), hero: HeroCreate):
-    resp = crud.hero.create(session, hero)
+def create_hero(*, db: Session = Depends(get_db), hero: HeroCreate):
+    resp = crud.hero.create(db, hero)
     return resp
+
+
+@router.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(*, db: Session = Depends(get_db), hero_id: int):
+    db_hero = crud.hero.get(db, hero_id)
+    if not db_hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return db_hero
+
+
+@router.patch("/heroes/{hero_id}", response_model=HeroRead)
+def update_hero(*, db: Session = Depends(get_db), hero_id: int, hero: HeroUpdate):
+    db_hero = crud.hero.get(db, hero_id)
+    if not db_hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+
+    return crud.hero.update(db, db_hero, hero)
+
+
+@router.delete("/heroes/{hero_id}")
+def delete_hero(*, db: Session = Depends(get_db), hero_id: int):
+    db_hero = crud.hero.get(db, hero_id)
+    if not db_hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    crud.hero.delete(db, db_hero)
+    return {"ok": True}
